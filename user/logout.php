d943251214b06e0411bc6ad218922609fee07885
Ianphantom <ian25yola@gmail.com>
2020-11-29 22:05:55 +0700

    perbaikan time pada logout

--- user/logout.php
+++ user/logout.php
@@ -1,10 +1,10 @@
 <?php 
     session_start();
     include("common/config.php");
+    //date_default_timezone_set("Asia/Kolkata");
+    //$ldate=date('d-m-Y h:i:s A', time());
+    mysqli_query($con, "UPDATE userlog set logout=CURRENT_TIMESTAMP() where username='".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
     $_SESSION['login']="";
-    date_default_timezone_set("Asia/Kolkata");
-    $ldate=date('d-m-Y h:i:s A', time());
-    mysqli_query($con, "UPDATE userlog set logout='$ldate' where username='".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
     session_unset();
 ?>
 <script>

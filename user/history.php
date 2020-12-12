7e9b4868dd561ef42477f10abc62e6637a3f7d5e
unknown <ian25yola@gmail.com>
2020-12-13 03:14:09 +0700

    Perbaikan

--- user/history.php
+++ user/history.php
@@ -20,19 +20,6 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/css/style.css">
-        <script>
-            function getCategory(value){
-                $.ajax({
-                    type: "POST",
-                    url: "subcategory.php",
-                    data:'category='+value,
-                    success: function(data){
-                        $("#subCategory").html(data);
-                        
-                    }
-                });
-            }
-        </script>
     </head>
 
     <body>

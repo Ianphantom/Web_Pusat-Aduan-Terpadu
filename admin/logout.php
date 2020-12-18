bafe5db81a53cd9d037f3ec8d938024055e7500e
unknown <ian25yola@gmail.com>
2020-12-19 05:22:30 +0700

    proses logout

---
+++ admin/logout.php
@@ -0,0 +1,12 @@
+<?php 
+    session_start();
+    include("common/config.php");
+    //date_default_timezone_set("Asia/Kolkata");
+    //$ldate=date('d-m-Y h:i:s A', time());
+    mysqli_query($con, "UPDATE admlog set logout=CURRENT_TIMESTAMP() where username='".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
+    $_SESSION['login']="";
+    session_unset();
+?>
+<script>
+    document.location="../admin/index.html";
+</script>

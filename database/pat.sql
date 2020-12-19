5064f08f593a3e7b1860d72164d914e674368535
unknown <ian25yola@gmail.com>
2020-12-19 09:17:09 +0700

    database V2

--- database/pat.sql
+++ database/pat.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 12, 2020 at 04:37 PM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2020 at 03:16 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,17 +24,105 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `adm`
+--
+
+CREATE TABLE `adm` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `update-date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `adm`
+--
+
+INSERT INTO `adm` (`id`, `username`, `password`, `update-date`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2020-12-15 04:38:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admlog`
+--
+
+CREATE TABLE `admlog` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `userip` binary(16) NOT NULL,
+  `loginTime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `logout` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admlog`
+--
+
+INSERT INTO `admlog` (`id`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
+(1, 'admin', 0x3a3a3100000000000000000000000000, '2020-12-18 22:17:40', '2020-12-19 05:17:44', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id_category` int(11) NOT NULL,
+  `jenis` varchar(256) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id_category`, `jenis`, `create_date`) VALUES
+(1, 'Pelayanan Publik', '2020-11-22 13:56:47'),
+(2, 'Sarana atau Prasarana', '2020-11-22 13:56:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar_komplain`
+--
+
+CREATE TABLE `daftar_komplain` (
+  `nomor_komplain` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `category` varchar(225) NOT NULL,
+  `subCategory` varchar(225) NOT NULL,
+  `masalah` varchar(225) NOT NULL,
+  `kecamatan` varchar(225) NOT NULL,
+  `detail` varchar(2000) NOT NULL,
+  `bukti` varchar(512) DEFAULT NULL,
+  `status` varchar(256) DEFAULT NULL,
+  `createDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updateTime` timestamp NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `daftar_komplain`
+--
+
+INSERT INTO `daftar_komplain` (`nomor_komplain`, `id_user`, `category`, `subCategory`, `masalah`, `kecamatan`, `detail`, `bukti`, `status`, `createDate`, `updateTime`) VALUES
+(1, 2, '1', 'Badan Penanggulangan Bencana Daerah', 'BPBD tidak tanggap', 'Laguboti', 'BPDB tidak tanggap dalam melakukan bantuan saat terjadi nya bencana banjir pada bulan desember', 'week1_Irwan Josafat_soal1.docx', NULL, '2020-12-12 20:52:11', '0000-00-00 00:00:00'),
+(2, 2, '1', 'Dinas Kesehatan', 'Covid-19', 'Laguboti', 'Petugas COVID 19 tidak cepat tanggap dalam melakukan karantina ', '13263954_10206510376143880_7764038578110869918_n.jpg', NULL, '2020-12-12 23:26:53', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `kecamatan`
 --
 
-DROP TABLE IF EXISTS `kecamatan`;
-CREATE TABLE IF NOT EXISTS `kecamatan` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `kecamatan` (
+  `id` int(11) NOT NULL,
   `nama` varchar(512) NOT NULL,
   `postingDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  `UpdateDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+  `UpdateDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `kecamatan`
@@ -51,20 +138,53 @@ INSERT INTO `kecamatan` (`id`, `nama`, `postingDate`, `UpdateDate`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `subcategory`
+--
+
+CREATE TABLE `subcategory` (
+  `id_sub` int(11) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  `jenis` varchar(256) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subcategory`
+--
+
+INSERT INTO `subcategory` (`id_sub`, `id_category`, `jenis`, `create_date`) VALUES
+(1, 1, 'Badan Penanggulangan Bencana Daerah', '2020-11-22 14:07:42'),
+(2, 1, 'Dinas Komunikasi dan Informatika', '2020-11-22 14:07:42'),
+(3, 1, 'Dinas Pendidikan', '2020-11-22 14:07:42'),
+(4, 1, 'Dinas Kesehatan', '2020-11-22 14:07:42'),
+(5, 1, 'Dinas Pekerjaan Umum dan Penataan Ruang', '2020-11-22 14:07:42'),
+(6, 1, 'Dinas Perumahan Rakyat dan Kawasan Permukiman', '2020-11-22 14:07:42'),
+(7, 1, 'Dinas Sosial', '2020-11-22 14:07:42'),
+(8, 1, 'Dinas Tenaga kerja', '2020-11-22 14:07:42'),
+(9, 1, 'Dinas Ketahanan Pangan', '2020-11-22 14:07:42'),
+(10, 1, 'Dinas Lingkungan Hidup', '2020-11-22 14:07:42'),
+(11, 2, 'Pendidikan', '2020-11-22 14:10:30'),
+(12, 2, 'Kesehatan', '2020-11-22 14:10:30'),
+(13, 2, 'RSUD Kota Porsea', '2020-11-22 14:10:30'),
+(14, 2, 'Pariwisata dan Kebudayaan', '2020-11-22 14:10:30'),
+(15, 2, 'Pertanian dan Perikanan', '2020-11-22 14:10:30'),
+(16, 2, 'Sosial', '2020-11-22 14:10:30');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `userlog`
 --
 
-DROP TABLE IF EXISTS `userlog`;
-CREATE TABLE IF NOT EXISTS `userlog` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `userlog` (
+  `id` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `userip` binary(16) NOT NULL,
   `loginTime` timestamp NOT NULL DEFAULT current_timestamp(),
   `logout` timestamp NULL DEFAULT NULL,
-  `status` int(11) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `userlog`
@@ -76,7 +196,18 @@ INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`,
 (3, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-01 15:06:36', NULL, 1),
 (4, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-01 15:10:52', '2020-12-01 16:15:08', 1),
 (5, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-01 16:22:10', NULL, 1),
-(6, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 16:23:37', NULL, 1);
+(6, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 16:23:37', NULL, 1),
+(7, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 16:58:20', NULL, 1),
+(8, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 17:30:12', NULL, 1),
+(9, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 19:37:57', NULL, 1),
+(10, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 20:29:21', '2020-12-12 21:56:58', 1),
+(11, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 22:05:15', '2020-12-12 22:05:51', 1),
+(12, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 22:05:55', '2020-12-12 22:10:04', 1),
+(13, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 22:10:08', '2020-12-12 22:26:41', 1),
+(14, 0, '', 0x3a3a3100000000000000000000000000, '2020-12-12 22:26:46', NULL, 0),
+(15, 0, '', 0x3a3a3100000000000000000000000000, '2020-12-12 22:26:50', NULL, 0),
+(16, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-12 22:26:53', '2020-12-12 23:38:33', 1),
+(17, 2, 'ian@gmail.com', 0x3a3a3100000000000000000000000000, '2020-12-13 10:29:50', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -84,9 +215,8 @@ INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`,
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
@@ -98,9 +228,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `image` varchar(255) DEFAULT NULL,
   `regDate` timestamp NOT NULL DEFAULT current_timestamp(),
   `updationDate` timestamp NULL DEFAULT current_timestamp(),
-  `status` int(1) DEFAULT 1,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `status` int(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
@@ -110,6 +239,110 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `nik`, `alamat`, `kecama
 (2, 'Ian Felix Jonathan Simanjutak', 'ian@gmail.com', '202cb962ac59075b964b07152d234b70', 1234123412341234, 'SMA UNGGUL YASOP Laguboti', 'Laguboti', 'Tobasa', 60111, '97ad8b6d0c08886a42fb10e6d4b95d64.jpg', '2020-11-25 12:45:48', '2020-11-25 12:45:48', 1),
 (3, 'coba2', 'kkk@gmail.com', '202cb962ac59075b964b07152d234b70', 1231231231231231, NULL, NULL, 'Tobasa', NULL, NULL, '2020-11-25 15:26:36', '2020-11-25 15:26:36', 1),
 (4, 'Ian Felix S', 'percobaan.tkk@gmail.com', '202cb962ac59075b964b07152d234b70', 1212312312312312, NULL, NULL, 'Tobasa', NULL, NULL, '2020-11-30 21:55:34', '2020-11-30 21:55:34', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adm`
+--
+ALTER TABLE `adm`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admlog`
+--
+ALTER TABLE `admlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id_category`);
+
+--
+-- Indexes for table `daftar_komplain`
+--
+ALTER TABLE `daftar_komplain`
+  ADD PRIMARY KEY (`nomor_komplain`);
+
+--
+-- Indexes for table `kecamatan`
+--
+ALTER TABLE `kecamatan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD PRIMARY KEY (`id_sub`);
+
+--
+-- Indexes for table `userlog`
+--
+ALTER TABLE `userlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `adm`
+--
+ALTER TABLE `adm`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `admlog`
+--
+ALTER TABLE `admlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `daftar_komplain`
+--
+ALTER TABLE `daftar_komplain`
+  MODIFY `nomor_komplain` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `kecamatan`
+--
+ALTER TABLE `kecamatan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  MODIFY `id_sub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `userlog`
+--
+ALTER TABLE `userlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
